12e91276d0ec3ee9442687c4b352a0d62f8dcfa7
VJ-07 <71382415+VJ-07@users.noreply.github.com>
2020-10-07 13:45:26 +0530

    Total Files

---
+++ index.js
@@ -0,0 +1,68 @@
+var noofbtn = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < noofbtn; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+
+    var htmlContent = this.innerHTML;
+
+    makeSound(htmlContent);
+    buttonAnimation(htmlContent);
+  });
+}
+
+
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      alert("invalid Key");
+
+  }
+}
+
+
+function buttonAnimation(currentKey){
+   var buttonPressed = document.querySelector("." + currentKey);
+   buttonPressed.classList.add("pressed");
+
+   setTimeout(function(){
+     buttonPressed.classList.remove("pressed");
+   }, 100);
+}
